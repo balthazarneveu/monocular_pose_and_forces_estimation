84338a05dc6d3101bda3bbeca7733e2aa5d1a719
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-16 01:08:19 +0100

    move simulation to dedicated header

--- studies/projectile/projectile.cc
+++ studies/projectile/projectile.cc
@@ -1,15 +1,7 @@
 #include <vector>
 #include <ceres/ceres.h>
 #include <Eigen/Core>
-
-// Gravity (m/s^2)
-const Eigen::Vector3d gravity(0, 0, -9.81);
-
-// Function to simulate position
-Eigen::Vector3d simulatePosition(const Eigen::Vector3d &initial_position, const Eigen::Vector3d &initial_velocity, double time)
-{
-    return 0.5 * gravity * time * time + initial_velocity * time + initial_position;
-}
+#include "simulate.h"
 
 // Cost function for Ceres Solver
 struct TrajectoryCostFunctor
