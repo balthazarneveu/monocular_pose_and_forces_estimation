d612d1985188e75039ce116544620f8a011ea1ac
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-18 14:27:52 +0100

    #6 fallback to moviepy decoding loop instead of buggy write_images_sequence

--- src/projectyl/video/decoder.py
+++ src/projectyl/video/decoder.py
@@ -1,16 +1,35 @@
 from pathlib import Path
 from moviepy.editor import VideoFileClip
 from typing import Optional
+import logging
+from projectyl.utils.io import Image
 
 def save_video_frames(input_path: Path, output_folder: Path, trim=None, resize: Optional[float]=None):
-    video = VideoFileClip(str(input_path))
-    if trim is not None:
-        video = video.subclip(*trim)
-    if resize is not None:
-        video = video.resize(resize)
-    if video.rotation in (90, 270): # Support vertical videos
-        # https://github.com/Zulko/moviepy/issues/586
-        video = video.resize(video.size[::-1])
-        video.rotation = 0
-    video_name = input_path.stem
-    video.write_images_sequence(str(output_folder/f'{video_name}_%05d.jpg'), logger='bar')
+    # video = VideoFileClip(str(input_path))
+    with VideoFileClip(str(input_path)) as video:
+        if resize is not None:
+            video = video.resize(resize)
+        if video.rotation in (90, 270): # Support vertical videos
+            # https://github.com/Zulko/moviepy/issues/586
+            video = video.resize(video.size[::-1])
+            video.rotation = 0
+        video_name = input_path.stem
+        
+        # video.write_images_sequence(str(output_folder/f'{video_name}_%05d.jpg'), logger='bar') *
+        # BUG DUPLCATED FRAMES!
+        
+        start, end = None, None
+        if trim is not None:
+            assert len(trim) == 2
+            start, end = trim
+            if start is not None:
+                start = int(start*video.fps)
+            if end is not None:
+                end = int(end*video.fps)
+        for frame_idx, frame in enumerate(video.iter_frames()):
+            if end is not None and frame_idx>end:
+                logging.info(f"LAST FRAME REACHED! {frame_idx}>{end}")
+                break
+            if start is not None and frame_idx<= start:
+                continue
+            Image.write(output_folder/f'{video_name}_{frame_idx:05d}.jpg', frame)
