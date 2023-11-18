13a1a517eb52751dd1d5d47126cc9ff8f5213e1b
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-18 10:06:07 +0100

    improve annotation function

--- src/projectyl/utils/segmentation_masks.py
+++ src/projectyl/utils/segmentation_masks.py
@@ -1,11 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
-def show_annotation(anns):
+def annotation_overlay(anns: dict, image: np.ndarray=None, ax: plt.Axes =None, show: bool=True):
+    """Add annotation overlay on top of current figure (or image)
+    """
+    if ax is None:
+        plt.figure()
+    if image is not None:
+        plt.imshow(image)
     if len(anns) == 0:
+        logging.info("no maks found")
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
+    if ax is None:
+        ax = plt.gca()
     ax.set_autoscale_on(False)
 
     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
@@ -15,3 +24,6 @@ def show_annotation(anns):
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
     ax.imshow(img)
+    ax.set_axis_off()
+    if show:
+        plt.show()
--- test/test_segmentation.py
+++ test/test_segmentation.py
@@ -1,19 +1,14 @@
 from segment_anything import SamAutomaticMaskGenerator
 from projectyl.algo.segmentation import load_sam_model
 from projectyl import root_dir
-
-
+from projectyl.utils.io import Image
+import matplotlib.pyplot as plt
+from projectyl.utils.segmentation_masks import annotation_overlay
+    
 # Basic check on sample image
-def test_sam(image_path=root_dir/"samples"/"sample_ball.jpg"):
-    from projectyl.utils.io import Image
+def test_sam(image_path=root_dir/"samples"/"sample_ball.jpg", show=False):
     image = Image.load(image_path)
     sam = load_sam_model()
     mask_generator = SamAutomaticMaskGenerator(sam)
-    _masks = mask_generator.generate(image)
-    # import matplotlib.pyplot as plt
-    # from projectyl.utils.segmentation_masks import show_annotation
-    # plt.figure()
-    # plt.imshow(image)
-    # show_annotation(masks)
-    # plt.axis('off')
-    # plt.show()
+    masks = mask_generator.generate(image)
+    annotation_overlay(masks, image=image, show=show)
