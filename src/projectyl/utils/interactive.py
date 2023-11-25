79ef8886cdce6e91b111b5df2117445f0e6502bd
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-25 15:50:20 +0100

    quick video viewer

--- src/projectyl/utils/interactive.py
+++ src/projectyl/utils/interactive.py
@@ -1,4 +1,5 @@
 from interactive_pipe import interactive, interactive_pipeline
+from interactive_pipe.data_objects.image import Image
 import numpy as np
 import logging
 from pathlib import Path
@@ -100,7 +101,7 @@ def trim_seq(sequence, unique_seq_name, start=0, end=1, global_params={}):
 @interactive(
     center_x=(0.5, [0., 1.], "cx", ["left", "right"]),
     center_y=(0.5, [0., 1.], "cy", ["up", "down"]),
-    size=(10., [6., 13., 0.3], "crop size", ["+", "-"])
+    size=(11., [6., 13., 0.3], "crop size", ["+", "-"])
 )
 def crop(image, center_x=0.5, center_y=0.5, size=8.):
     # size is defined in power of 2
@@ -133,7 +134,12 @@ def crop(image, center_x=0.5, center_y=0.5, size=8.):
         crop = image[..., start_y:end_y, start_x:end_x]
     if offset == 1:
         crop = image[..., start_y:end_y, start_x:end_x, :]
-    return cv.resize(crop, (w, h))
+    MAX_ALLOWED_SIZE = 512
+    w_resize = int(min(MAX_ALLOWED_SIZE, w))
+    h_resize = int(w_resize/w*h)
+    h_resize = int(min(MAX_ALLOWED_SIZE, h_resize))
+    w_resize = int(h_resize/h*w)
+    return cv.resize(crop, (w_resize, h_resize))
 
 
 def visualize(sequence):
@@ -206,25 +212,30 @@ def live_view(video_path: Path,
         dict: information on trimming
     """
     # Preload video in RAM
-    assert video_path.exists(), f"Video path {video_path} does not exist"
-    if preload_ram:
-        video = VideoFileClip(str(video_path))
-        if video.rotation in (90, 270):  # Support vertical videos
-            video = video.resize(video.size[::-1])
-            video.rotation = 0
-        total_frames = int(video.duration*video.fps)-1
-        full_decoded_video_in_ram = [
-            cv.resize(video.get_frame(idx*1.0/video.fps), None, fx=resize, fy=resize)/255.
-            for idx in tqdm(range(0, total_frames, skip_frames), desc=f"Pre decoding video {video_path.name}")
-        ]
-        video.close()
-    else:
-        full_decoded_video_in_ram = str(video_path)
+    if isinstance(video_path, str) or isinstance(video_path, Path):
+        video_path = Path(video_path)
+        assert video_path.exists(), f"Video path {video_path} does not exist"
+        if preload_ram:
+            video = VideoFileClip(str(video_path))
+            if video.rotation in (90, 270):  # Support vertical videos
+                video = video.resize(video.size[::-1])
+                video.rotation = 0
+            total_frames = int(video.duration*video.fps)-1
+            full_decoded_video_in_ram = [
+                cv.resize(video.get_frame(idx*1.0/video.fps), None, fx=resize, fy=resize)/255.
+                for idx in tqdm(range(0, total_frames, skip_frames), desc=f"Pre decoding video {video_path.name}")
+            ]
+            video.close()
+        else:
+            full_decoded_video_in_ram = str(video_path)
+    elif isinstance(video_path, list) or isinstance(video_path, tuple):
+        full_decoded_video_in_ram = np.array(
+            [Image.load_image(img).data for img in tqdm(
+                video_path, desc=f"Loading images {Path(video_path[0]).parent.parent.name}")])
 
     selected_frames = {}
     if trimming:
         selected_frames = interactive_trimming(full_decoded_video_in_ram, video_path.name)
     else:
-        int_viz = interactive_pipeline(gui="auto", cache=True)(visualize)
-        int_viz(full_decoded_video_in_ram, global_params={})
+        interactive_visualize(full_decoded_video_in_ram)
     return selected_frames
