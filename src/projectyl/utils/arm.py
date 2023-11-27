7e7b5f808cec3e4cfa5fb09c59632a18b8a4b4f0
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-27 22:29:36 +0100

    pimp replay_sequence with curves

--- src/projectyl/utils/arm.py
+++ src/projectyl/utils/arm.py
@@ -3,7 +3,9 @@ import numpy as np
 import logging
 from time import sleep
 from interactive_pipe import interactive, interactive_pipeline
+from interactive_pipe.data_objects.curves import Curve, SingleCurve
 import matplotlib.pyplot as plt
+FROZEN_COLOR_CODE = ["r", "g", "b", "y", "m", "c", "k"]
 
 
 def retrieve_arm_estimation(body_pose_full: list, frame_idx: int, arm_side: str = LEFT) -> dict:
@@ -37,21 +39,57 @@ def replay_whole_sequence(q_list, viz):
 
 
 @interactive(
-    frame_idx=(0., [0., 1.], "frame_idx"),
+    # frame_idx=(0., [0., 1.], "frame_idx",  0.000001),
+    frame_idx=[0, 500, 1, 0],
     mode=(0, [0, 10], "index [0 for estimation -1 for GT]"),
 )
-def interactive_replay(q_dict, viz, frame_idx=0., mode=0, global_params={}):
+def seclect_replay(q_dict, frame_idx=0., mode=0, global_params={}):
     q_keys = list(q_dict.keys())
     q_key = q_keys[min(mode, len(q_keys)-1)]
     q_list = q_dict[q_key]
     logging.info(f"Replay mode {q_key} at index {frame_idx}")
-    q = q_list[int(frame_idx*(len(q_list)-1))]
+    global_params["frame_idx"] = int(min(frame_idx, len(q_list)-1))  # int(frame_idx*(len(q_list)-1))
+    global_params["q_key"] = q_key
+    return q_list,
+
+
+@interactive()
+def meshcat_replay(q_list, viz,  global_params={}):
+    frame_idx = global_params["frame_idx"]
+    q = q_list[frame_idx]
     viz.display(q)
-    # sleep(1/30.)
+
+
+@interactive()
+def display_curve(q_dict, global_params={}):
+    ts = global_params["frame_idx"]
+    q_key = global_params["q_key"]
+    q_array = np.array(q_dict[q_key])
+    curves = [
+        SingleCurve(
+            y=q_array[:, dim],
+            style="-"+FROZEN_COLOR_CODE[dim],
+            label=f"{q_key} shoulder {dim}")
+        for dim in range(3)
+    ]
+    curves.append(SingleCurve(
+        y=q_array[:, 4],
+        style="-"+FROZEN_COLOR_CODE[3],
+        label=f"{q_key} elbow"))
+    curves.append(SingleCurve(x=[ts, ts], y=[-1, 1], style="--k", label=None))
+    return Curve(
+        curves,
+        title="Q state [rad]", xlabel=f"Time index - {ts:d}", ylabel="Q [rad]",
+        grid=True
+    )
 
 
 def replay_sequence(q_dict, viz):
-    interactive_replay(q_dict, viz)
+    q_list = seclect_replay(q_dict)
+    meshcat_replay(q_list, viz)
+    curve = display_curve(q_dict)
+
+    return curve
 
 
 def interactive_replay_sequence(q_dict, viz):
@@ -63,32 +101,31 @@ def plot_optimization_curves(states_to_plot: list, mode: str = "qvt", title: str
     fig, axs = plt.subplots(1, n_fig, figsize=(n_fig*5, 5))
     if n_fig == 1:
         axs = [axs]
-    frozen_color_code = ["r", "g", "b", "y", "m", "c", "k"]
 
     for state, label, style in states_to_plot:
         graph_id = 0
         if "q" in mode:
             ax = axs[graph_id]
             for dim in range(3):
-                ax.plot(state[:, 0+dim], style+frozen_color_code[dim],
+                ax.plot(state[:, 0+dim], style+FROZEN_COLOR_CODE[dim],
                         label=f"q shoulder {label} {dim}")  # skip index 3 (quaternion normalization)
-            ax.plot(state[:, 4], style+frozen_color_code[3], label=f"q elbow {label}")
+            ax.plot(state[:, 4], style+FROZEN_COLOR_CODE[3], label=f"q elbow {label}")
             ax.set_title("Q state [rad] (?)")
             graph_id += 1
         if "v" in mode:
             ax = axs[graph_id]
             for dim in range(3):
-                ax.plot(state[:, 4+1+dim], style+frozen_color_code[dim], label=f"q' shoulder {label} {dim}")
-            ax.plot(state[:, 4+3+1], style+frozen_color_code[3], label=f"q' elbow {label}")
+                ax.plot(state[:, 4+1+dim], style+FROZEN_COLOR_CODE[dim], label=f"q' shoulder {label} {dim}")
+            ax.plot(state[:, 4+3+1], style+FROZEN_COLOR_CODE[3], label=f"q' elbow {label}")
             ax.set_title("Velocity [rad/s] (?)")
             graph_id += 1
         if "t" in mode:
             ax = axs[graph_id]
             for dim in range(3):
-                ax.plot(state[:, 4+3+1+dim], style+frozen_color_code[dim],
+                ax.plot(state[:, 4+3+1+dim], style+FROZEN_COLOR_CODE[dim],
                         label=f"Torque shoulder {label} {dim}")
             ax.set_title("Torque")
-            ax.plot(state[:, 4+3+1], style+frozen_color_code[3], label=f"Torque elbow {label}")
+            ax.plot(state[:, 4+3+1], style+FROZEN_COLOR_CODE[3], label=f"Torque elbow {label}")
             graph_id += 1
     for i in range(len(axs)):
         axs[i].legend()
